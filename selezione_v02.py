4eb519ae9757a859347e7cab80000526155e407f
gabriele-salvai <gabriele.salvai@iismajoranamoncalieri.edu.it>
2024-11-18 12:55:45 +0000

    gg

--- selezione_v02.py
+++ selezione_v02.py
@@ -1,9 +1,9 @@
 
 reddito=float(input("inserisci il tuo redditto:\t"))
 if reddito<15000:
-    aliquota= (15000*0.12)
+    aliquota= (reddito*0.12)
 else:
-    aliquota= (15000*0.22)
+    aliquota= (reddito*0.22)
 print("l'mposta dovuta è pari a:", aliquota, "€")
 
 
